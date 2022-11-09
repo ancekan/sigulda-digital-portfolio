8f9a1b1c11df8c474332ec8a27657f24d5e7512c
ancekan <58723696+ancekan@users.noreply.github.com>
2022-11-09 16:21:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 $(document).ready(function() { 
 
-    $("#body_container").css("display", "block");
-    $("#image_container").css("display", "none");
-    $("#form_content").css("display", "none");
+var settings = {
+"bodyContainerDisplay": "block",
+"imageContainerDislay": "none",
+"formContentDisplay": "none",
+};
 
-    $("#header").css("background", "black");
+    $("#body_container").css("display", settings.bodyContainerDisplay);
+    $("#image_container").css("display", settings.imageContainerDislay);
+    $("#form_content").css("display", settings.formContentDisplay);
+
+    $("#header").css("background", "white");
     $("#header").css("width", "100%");
     $("#header").css("height", "20%");
 
@@ -30,5 +36,68 @@ $(document).ready(function() {
 
     $("#image_content").css("padding-top", "100px");
     $("#form_content").css("padding-top", "100px");
+
+    $("#navigation_buttons").css("color","red");
+
+   // $("#body_container").css("width","1000px");
+   // $("#body_container").css("height","500px");
+    
+    
+
+    $("#about_me").css("display","inline-block");
+    $("#about_me").css("padding","15px 25px");
+    $("#about_me").css("font-size","24px");
+    $("#about_me").css("cursor","pointer");
+    $("#about_me").css("text-align","center");
+    $("#about_me").css("text-decoration","none");
+    $("#about_me").css("outline","none");
+    $("#about_me").css("color","#fff");
+    $("#about_me").css("background-color","#4CAF50");
+    $("#about_me").css("border","none");
+    $("#about_me").css("border-radius","15px");
+    $("#about_me").css("box-shadow","0 9px #999");
+
+    
+
+   // $("#about_me").css("border-style","solid");
+  //  $("#about_me").css("border-color","blue");
+
+    $("#navigation_buttons p").click(function(){
+        $(this).css("color","blue");
+
+        var text = $(this).text();
+
+        if(text == "Par mani") {
+            $("#body_container").css("display", "block");
+            $("#image_container").css("display", "none");
+            $("#form_content").css("display", "none");
+            $("#my_works").css("color","red");
+            $("#info").css("color","red");
+            
+            //$("#about_me").css("background-color","#3e8e41");
+          //  $("#about_me").css("box-shadow","0 5px #666");
+          //  $("#about_me").css("transform","translateY(4px)");
+            
+        }
+
+        if(text == "Mani darbi") {
+            $("#body_container").css("display", "none");
+            $("#image_container").css("display", "block");
+            $("#form_content").css("display", "none");
+            $("#about_me").css("color","red");
+            $("#info").css("color","red");
+        }
+
+        if(text == "Kontaktinformācija") {
+            $("#body_container").css("display", "none");
+            $("#image_container").css("display", "none");
+            $("#form_content").css("display", "block");
+            $("#about_me").css("color","red");
+            $("#my_works").css("color","red");
+        }
+
+    });
+
+    
 
 });
